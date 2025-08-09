3bca5a2c1b37ee8e134c90d65239576e1577e611
Andres Mauricio Quintero Rios <Quintero2273@hotmail.com>
2025-08-08 23:30:58 -0500

    fin de la repo  con extenciones e interfaces

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+    
+            
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Polimorfismo
+        Animal miperro = new Perro("Pancho", "Chandoberman", "Reino Animal");
+        miperro.hacerSonido();
+
+        // Abstraccion
+        Perro perro = new Perro("Gonzo", "Dogo", "Reino Animal");
+        Gato gato = new Gato("Michi", "Persa", "Reino Animal");
+        perro.correr(32.5);
+        gato.correr(16.3);
+        gato.hacerSonido(); // Miau!!
+        
     }
 }
---
+++ src/Reinos.java
@@ -0,0 +1,18 @@
+
+public abstract class Reinos {
+
+    public String nombreReino;
+
+    public Reinos(String nombreReino) {
+        this.nombreReino = nombreReino;
+    }
+
+    public void monstrarReino() {
+        System.out.println("Es el Reino : " + nombreReino);
+    }
+
+    abstract void MostrarNombreCientifico(String nombreCientifico);
+
+}
+    
+
